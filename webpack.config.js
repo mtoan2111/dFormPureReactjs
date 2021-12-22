5c42a392b3f41b5a3f0fa5458947acb41447eedd
mtoan2111 <toan93.hust@gmail.com>
2021-12-22 18:51:39 +0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require('webpack')
 
 const config = {
     entry: "./src/index.js",
@@ -34,6 +35,11 @@ const config = {
             },
         ],
     },
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env.NODE_ENV": JSON.stringify("development"),
+        }),
+    ],
 };
 
 module.exports = config;
